a654c4a2c99255de3523d0818cfed79b23f3cea2
Koravit <krocata@kkumail.com>
2022-01-18 11:49:56 +0700

    basic flask

--- testflask.py
+++ testflask.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, request ,  render_template
+import json
 
 app = Flask(__name__)
 
 @app.route("/")
-def home():
+def hello():
     return "Hello, World!"
     
 @app.route("/name")
@@ -18,5 +19,13 @@ def request_detail():
 
     print(inmassage)
 
+    json_data = json.dumps({"y":"received"})
+    return json_data
+
+##webapp
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
 if __name__ == "__main__":
-    app.run()#host ='0.0.0.0'
+    app.run()# host ='0.0.0.0',port=5001 
