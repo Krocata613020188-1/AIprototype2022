0f2d348c8c6b0af2c4cd07f2814e430879a8edb0
Koravit <krocata@kkumail.com>
2022-01-19 19:39:18 +0700

    edit login

--- testflask3.py
+++ testflask3.py
@@ -8,5 +8,12 @@ app = Flask(__name__)
 def home():
     return render_template("web2.html")
 
+@app.route("/")
+def login():
+    return render_template("login.html")
+
+@app.route("/show")
+def show():
+    return render_template("showdata.html")
 if __name__ == "__main__":
     app.run(host ='0.0.0.0',port=5001)# host ='0.0.0.0',port=5001 
