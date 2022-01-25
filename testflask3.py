f2067f03e7c84049f933a2253e83a43810fee0ea
Koravit <krocata@kkumail.com>
2022-01-25 12:04:31 +0700

    edit web

--- testflask3.py
+++ testflask3.py
@@ -1,10 +1,14 @@
-from flask import Flask, request ,  render_template
+
+from flask import Flask, request ,  render_template,redirect,url_for
 import json
+import pymysql
 
 app = Flask(__name__)
-
+conn = pymysql.connect(host='localhost',user='root',
+                             password='',
+                             database='testweb')
 ##webapp
-@app.route("/home")
+@app.route("/adddata")
 def home():
     return render_template("web2.html")
 
@@ -14,6 +18,26 @@ def login():
 
 @app.route("/show")
 def show():
-    return render_template("showdata.html")
+    with conn:
+        cur = conn.cursor()
+        cur.execute("select * from testdb1")
+        rows = cur.fetchall()
+    return render_template("connect.html",datas = rows)
+
+@app.route("/insert",methods = ['POST'])
+def insert():
+    if request.method =="POST":
+        car_id = request.form['car_id']
+        Sname = request.form['Sname']
+        insurance = request.form['insurance']
+        
+        with conn.cursor() as cursor:
+            sql = "Insert into `testdb1` (`car_id`,`Sname`,`insurance`) values(%s,%s,%s)"
+            cursor.execute(sql,(car_id,Sname,insurance))
+            conn.commit()
+        return redirect(url_for('show'))
+
+
+
 if __name__ == "__main__":
-    app.run(host ='0.0.0.0',port=5001)# host ='0.0.0.0',port=5001 
+    app.run()# host ='0.0.0.0',port=5001 
